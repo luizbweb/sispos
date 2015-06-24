7b7ab3cebde1c8d3f25303973fc3bbde2961bf8f
Leonardo Cordeiro <leonardo.cordeiro@caelum.com.br>
2015-06-24 12:45:23 -0300

    mais modelo

---
+++ src/main/java/org/lema/sispos/modelo/Diario.java
@@ -0,0 +1,9 @@
+package org.lema.sispos.modelo;
+
+import java.util.List;
+
+public class Diario {
+	List<Presenca> presencas = null;
+	Turma turma;
+	
+}
---
+++ src/main/java/org/lema/sispos/modelo/Presenca.java
@@ -0,0 +1,9 @@
+package org.lema.sispos.modelo;
+
+import java.util.Calendar;
+
+public class Presenca {
+	Aluno aluno;
+	Calendar hoje;
+	
+}
