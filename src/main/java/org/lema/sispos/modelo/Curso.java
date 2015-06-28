98834921e5981310dbbc01be62ecfe4c6f8339f6
maxalves <max.phazen@gmail.com>
2015-06-28 13:56:52 -0300

    Enum para Areas de Conhecimento
    Primeira versão do Enum para Areas de Conhecimento

--- src/main/java/org/lema/sispos/modelo/Curso.java
+++ src/main/java/org/lema/sispos/modelo/Curso.java
@@ -1,5 +1,16 @@
 package org.lema.sispos.modelo;
 
 public class Curso {
+	String nome, identificacao;
+	int duracao;
+	
+	public enum AreasConhecimento {
+		Exatas(1), Biologicas(2), Saude(3), Agrarias(4), Sociais(5), Humanas(6), Engenharias(7), linguistiticas(8), Outros(9);
+	
+	public int valorCarta;
+	
+	AreasConhecimento(int valor) {
+		valorCarta = valor; }
+	}
 
 }
