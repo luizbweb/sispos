8988b40cf5b064105fbee248bad341fb562a2c59
maxalves <max.phazen@gmail.com>
2015-06-28 14:02:30 -0300

    Enum
    Construção do Enum

--- src/main/java/org/lema/sispos/modelo/Curso.java
+++ src/main/java/org/lema/sispos/modelo/Curso.java
@@ -4,13 +4,20 @@ public class Curso {
 	String nome, identificacao;
 	int duracao;
 	
+	//Em construcao!
 	public enum AreasConhecimento {
-		Exatas(1), Biologicas(2), Saude(3), Agrarias(4), Sociais(5), Humanas(6), Engenharias(7), linguistiticas(8), Outros(9);
+		Coordenacao(0), Exatas(1), Biologicas(2), Saude(3), Agrarias(4),
+		Sociais(5), Humanas(6), Engenharias(7), linguistiticas(8), Outros(9);
 	
-	public int valorCarta;
+		private int valor;
 	
-	AreasConhecimento(int valor) {
-		valorCarta = valor; }
+		AreasConhecimento(int valorOpcao){
+			this.valor = valorOpcao;
+		}
+	
+		public int getValor(){
+			return valor;
+		}
 	}
 
 }
