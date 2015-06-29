0f276fe42e41ffa5127557178ecdb16cb4d95b4e
Leonardo Cordeiro <leonardo.cordeiro@caelum.com.br>
2015-06-29 00:12:42 -0300

    apagando import

--- src/main/java/org/lema/sispos/controller/HomeController.java
+++ src/main/java/org/lema/sispos/controller/HomeController.java
@@ -1,6 +1,5 @@
 package org.lema.sispos.controller;
 
-import org.lema.sispos.modelo.Curso;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
