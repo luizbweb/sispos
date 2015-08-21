3692468f6567aab3807a9d3004af7e98795030d6
IgorRamosBR <igorrpestana@yahoo.com.br>
2015-08-21 14:28:51 -0300

    criacao disciplinaXController

---
+++ src/main/java/org/lema/sispos/controller/DisciplinaController.java
@@ -0,0 +1,15 @@
+package org.lema.sispos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/disciplina")
+public class DisciplinaController {
+	
+	@RequestMapping("/form");
+	public String form(){
+		return "disciplina/form";
+	}
+
+}
