27229cbad7051cd1e7fef021fea55fc29b764a60
Leonardo Cordeiro <leonardo.cordeiro@caelum.com.br>
2015-08-31 03:27:28 -0300

    adicionando persistencia

--- src/main/java/org/lema/sispos/controller/AlunoController.java
+++ src/main/java/org/lema/sispos/controller/AlunoController.java
@@ -4,6 +4,7 @@ import org.lema.sispos.dao.AlunoDao;
 import org.lema.sispos.model.Aluno;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,9 +20,10 @@ public class AlunoController {
 		return "aluno/form";
 	}
 	
+	@Transactional
 	@RequestMapping(method=RequestMethod.POST, name="cadastrarAluno")
 	public String save(Aluno aluno) {
-		//alunoDao.salvar(aluno);
+		alunoDao.salvar(aluno);
 		
 		return "redirect:/";
 	}
