30162b3072879e3baa348972df9212d2fc41cc23
LucasGarcez <lucan1.fla@gmail.com>
2015-09-08 20:00:01 -0300

    arrumando erros e add resources

--- src/main/java/org/lema/sispos/controller/AlunoController.java
+++ src/main/java/org/lema/sispos/controller/AlunoController.java
@@ -2,12 +2,13 @@ package org.lema.sispos.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.lema.sispos.dao.AlunoDao;
 import org.lema.sispos.model.Aluno;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
