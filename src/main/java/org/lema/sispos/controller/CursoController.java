7afd227c91e8f98a145f7b79b4670069855a6a0f
LucasGarcez <lucan1.fla@gmail.com>
2015-09-11 12:20:23 -0300

    ModelAndView cursoController

--- src/main/java/org/lema/sispos/controller/CursoController.java
+++ src/main/java/org/lema/sispos/controller/CursoController.java
@@ -1,5 +1,7 @@
 package org.lema.sispos.controller;
 
+import java.util.List;
+
 import org.lema.sispos.dao.CursoDao;
 import org.lema.sispos.model.Curso;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/curso")
@@ -26,6 +29,16 @@ public class CursoController {
 		cursoDao.salvar(curso);
 		
 		return "redirect:/";
+	}
+		
+		@RequestMapping(value="lista",method=RequestMethod.GET)
+		public ModelAndView list() { 
+			List<Curso> cursos = cursoDao.lista();
+			
+			ModelAndView modelAndView = new ModelAndView("curso/lista");
+			modelAndView.addObject("cursos", cursos);
+			
+			return modelAndView;
 	}
 
 }
